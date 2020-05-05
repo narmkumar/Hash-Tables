c4bfafaa80ba41d0992686ac604440095203dbd3
Nar Kumar <njn.kumar@gmail.com>
2020-05-05 09:45:34 -0700

    day 1

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -13,64 +13,74 @@ class HashTable:
     """
     A hash table that with `capacity` buckets
     that accepts string keys
-
     Implement this.
     """
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.hash_table = [None] * capacity
 
     def fnv1(self, key):
         """
         FNV-1 64-bit hash function
-
         Implement this, and/or DJB2.
         """
 
     def djb2(self, key):
         """
         DJB2 32-bit hash function
-
         Implement this, and/or FNV-1.
         """
+        hash = 5381
+        for c in key:
+            hash = (hash * 33) + ord(c)
+        return hash
 
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
 
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Implement this.
         """
+        index = self.hash_index(key)
+
+        if self.hash_table[index] is None:
+            self.hash_table[index] = HashTableEntry(key, value)
+        else:
+            current_node = self.hash_table[index]
+            self.hash_table[index] = HashTableEntry(key, value)
+            self.hash_table[index].next = current_node
+
 
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Implement this.
         """
+
+        index = self.hash_index(key)
+        self.hash_table[index] = None
 
     def get(self, key):
         """
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Implement this.
         """
+        index = self.hash_index(key)
+        return self.hash_table[index].value
 
     def resize(self):
         """
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
-
         Implement this.
         """
 
